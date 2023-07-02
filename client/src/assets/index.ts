e28c8cbbc1ac55685eb365b6a102bbfe122badfc
siddhantprateek <siddhantprateek@gmail.com>
2023-07-03 02:47:33 +0530

    ft(client): migrated the client-side

--- client/src/assets/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as ARROW } from './arrow.png';
-export { default as BACK } from './back.png';
-export { default as OPTION } from './option.png';
--- client/src/components/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as Header } from './header/header.components';
-export { default as Hero } from './hero/hero.components';
